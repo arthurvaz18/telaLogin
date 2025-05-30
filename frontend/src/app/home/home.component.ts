c928d1b5258fbb3e527a9828187be16f24f841dd
arthur <arthur.fernandes@sonner.com.br>
2025-05-30 13:39:16 -0300

    Trabalhando Properties Binding e Change Detection

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -8,23 +8,7 @@ import { HomeService} from "./home.service";
 })
 export class HomeComponent implements OnInit {
 
-  url: string = "http/loaine.com"
 
-  contador = 0;
-
-  cursoAngular: boolean = true
-
-  urlImagem = 'https://picsum.photos/200/300';
-
-  getCursoAngular(){
-    return true;
-  }
-  getValor(){
-    return 1;
-  }
-  incrementaValor(){
-    this.contador ++;
-  }
   constructor(private mainService: HomeService,
               private injector: Injector) {
   }
