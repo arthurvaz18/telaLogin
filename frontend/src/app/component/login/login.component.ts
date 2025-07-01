84f52bbc768646527be5581d7e652116812e8dd1
arthur <arthur.fernandes@sonner.com.br>
2025-07-01 15:31:00 -0300

    Tela de Login criada

--- frontend/src/app/component/login/login.component.ts
+++ frontend/src/app/component/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LoginService} from "../../services/login.service";
+import {Router} from "@angular/router";
+import {Estabelecimento} from "../../models/estabelecimento.model";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  estabelecimento: Estabelecimento = new Estabelecimento();
+
+  constructor(private mainService: LoginService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  login() {
+    if(this.estabelecimento.email && this.estabelecimento.senha){
+      this.mainService.logarEstabelecimento(this.estabelecimento).subscribe({
+        next: (logar) =>{
+        alert('Login realizado com Sucesso');
+        this.router.navigate(['/inicio']);
+        },
+        error:(erro) =>{
+          console.error('Erro ao Logar', erro);
+          alert('Email ou senha inválidos. Tente novamente.');
+      }
+      });
+    }else {
+      alert('Preencha email e senha');
+    }
+  }
+
+  CriarCadastro() {
+    this.router.navigate(['/cadastro'])
+  }
 }
