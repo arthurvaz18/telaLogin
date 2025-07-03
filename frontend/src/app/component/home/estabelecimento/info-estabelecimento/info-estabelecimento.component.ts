31ade76c230d3381346bfb209543125772ec7b83
arthur <arthur.fernandes@sonner.com.br>
2025-07-03 11:52:31 -0300

    Criando um SubMenu para o EstabelecimentoComponent

---
+++ frontend/src/app/component/home/estabelecimento/info-estabelecimento/info-estabelecimento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-estabelecimento',
+  templateUrl: './info-estabelecimento.component.html',
+  styleUrls: ['./info-estabelecimento.component.scss']
+})
+export class InfoEstabelecimentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
